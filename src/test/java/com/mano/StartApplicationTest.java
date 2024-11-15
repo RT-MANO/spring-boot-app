35424b6e49dd8cf85493d1ed51bc624cd2eb06cf
RT-MANO <rt_mano@yahoo.com>
2024-11-15 12:27:17 +0800

    Display content changed

--- src/test/java/com/mano/StartApplicationTest.java
+++ src/test/java/com/mano/StartApplicationTest.java
@@ -1,48 +1,27 @@
 package com.mano;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.springframework.ui.Model;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.ModelAndView;
 
-@WebMvcTest(StartApplication.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class StartApplicationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private StartApplication startApplication;
-
     @Test
     public void testIndex() throws Exception {
         mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"))
+                .andExpect(status().isOk()) // HTTP 200 status
+                .andExpect(view().name("index")) // View name
                 .andExpect(model().attribute("title", "I have deployed a sprint boot application using GITOPS CI/CD"))
                 .andExpect(model().attribute("msg", "This application is deployed on to Minikube K38 cluster using Argo CD"));
     }
-
-    @Test
-    public void testIndex1() {
-        Model model = null; // Mock Model if needed
-        ModelAndView mv = startApplication.index(model);
-
-        // Verify view name
-        assertEquals("index", mv.getViewName());
-
-        // Verify model attributes
-        assertThat(mv.getModel(), hasEntry("title", "I have deployed a sprint boot application using GITOPS CI/CD"));
-        assertThat(mv.getModel(), hasEntry("msg", "This application is deployed on to Minikube K38 cluster using Argo CD"));
-    }
-    }
+}
