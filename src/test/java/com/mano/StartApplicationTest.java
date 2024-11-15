b647273a263caade120233106587cec3758a0534
RT-MANO <rt_mano@yahoo.com>
2024-11-15 12:57:22 +0800

    Display content changed

--- src/test/java/com/mano/StartApplicationTest.java
+++ src/test/java/com/mano/StartApplicationTest.java
@@ -1,27 +1,13 @@
 package com.mano;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-public class StartApplicationTest {
+class StartApplicationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-/*
     @Test
-    public void testIndex() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk()) // HTTP 200 status
-                .andExpect(view().name("index")) // View name
-                .andExpect(model().attribute("title", "I have deployed a sprint boot application using GITOPS CI/CD"))
-                .andExpect(model().attribute("msg", "This application is deployed on to Minikube K38 cluster using Argo CD"));
-    }*/
+    void contextLoads() {
+    }
+
 }
