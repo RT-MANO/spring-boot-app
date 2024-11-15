f387b034ab668d6a0bc90820835e775e7aed4c82
RT-MANO <rt_mano@yahoo.com>
2024-11-15 11:30:55 +0800

    Display content changed

--- src/test/java/com/mano/StartApplicationTest.java
+++ src/test/java/com/mano/StartApplicationTest.java
@@ -1,12 +1,26 @@
 package com.mano;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-class StartAppplicationTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(StartApplication.class)
+public class StartApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("title", "I have deployed a sprint boot application using GITOPS CI/CD"))
+                .andExpect(model().attribute("msg", "This application is deployed on to Minikube K38 cluster using Argo CD"));
     }
+
 }
