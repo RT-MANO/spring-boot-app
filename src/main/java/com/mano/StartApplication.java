9705398ed72d1a40b3c068ddc5f93e47a94cf2f1
RT-MANO <rt_mano@yahoo.com>
2024-11-17 18:51:00 +0800

    Display content changed

--- src/main/java/com/mano/StartApplication.java
+++ src/main/java/com/mano/StartApplication.java
@@ -18,7 +18,7 @@ public class StartApplication {
     public ModelAndView index(final Model model) {
         ModelAndView mv = new ModelAndView("index");
         mv.addObject("title", "I have deployed a sprint boot application using GITOPS CI/CD");
-        mv.addObject("msg", "This application is deployed on to Minikube K38 cluster using Argo CD");
+        mv.addObject("msg", "This application is deployed on to AWS EKS cluster using Argo CD");
         return mv;
     }
 }
